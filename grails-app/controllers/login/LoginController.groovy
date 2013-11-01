cc5431d7372c0fc895b44003aacc65b9dc55bff8
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2013-11-01 22:15:49 +0100

    controller test

--- grails-app/controllers/login/LoginController.groovy
+++ grails-app/controllers/login/LoginController.groovy
@@ -4,27 +4,63 @@ import org.pac4j.springframework.security.authentication.ClientAuthenticationTok
 import org.springframework.security.core.context.SecurityContextHolder
 import grails.converters.*
 
+import org.pac4j.springframework.security.authentication.*
+import org.springframework.security.core.*
+import org.springframework.security.core.context.SecurityContextHolder
+import org.pac4j.core.context.*
+import org.pac4j.core.profile.*
+import org.pac4j.http.client.*
+import org.pac4j.oauth.client.*
+import org.pac4j.cas.client.*
+import org.pac4j.openid.client.*
+
+import static login.SpringUtil.*
+
 class LoginController {
 
+    SecurityContextHolder sch
 
 	def index() {
 		render "HelloWorld"
 	}
 
 	def applicationContext() {
-		def bean = SpringUtil.getBean("servletContextAttributeExporter")
+		def bean = getBean("servletContextAttributeExporter")
 		
 		if(!bean.getProperties().empty)
 			for(s in bean.getProperties())
 				println s
 	}
 
-	
-	def facebook(){
-		ClientAuthenticationToken token = (ClientAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-		def profile = token.getUserProfile();
-		render(profile as JSON)
-	}
+
+    def param(){
+             //test id of controller action
+             render params.id
+    }
+
+    def auth(){
+        WebContext context = new J2EContext(request, response);
+        FacebookClient fbClient = (FacebookClient)getBean("servletContextAttributeExporter").getProperties().get("FacebookClient")
+
+
+        ClientAuthenticationToken token = (ClientAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        render token.getUserProfile()
+    }
+
+    def profile(){
+
+        Authentication auth = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+        UserProfile profile = null;
+        if (auth != null && auth instanceof ClientAuthenticationToken) {
+            ClientAuthenticationToken token = (ClientAuthenticationToken) auth;
+            profile = token.getUserProfile();
+            render profile
+        }
+
+        render "no profile"
+    }
+
+
 	
 
 }
